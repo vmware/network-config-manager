71dc712c6c9c1e0cefcfa9becb4005a5eb60d998
Susant Sahani <ssahani@vmware.com>
2020-09-03 04:30:26 +0000

    Reload conf file after changes to any conf

--- src/manager/network-manager.c
+++ src/manager/network-manager.c
@@ -98,7 +98,7 @@ int manager_set_link_mode(const IfNameIndex *ifnameidx, bool mode, char **ret) {
         if (r < 0)
                 return r;
 
-        r = dbus_reconfigure_link(ifnameidx->ifindex);
+        r = dbus_network_reload(ifnameidx->ifindex);
         if (r < 0)
                 return r;
 
@@ -132,7 +132,7 @@ int manager_set_link_dhcp_mode(const IfNameIndex *ifnameidx, DHCPMode mode) {
                 return r;
         }
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_set_link_dhcp_client_identifier(const IfNameIndex *ifnameidx, DHCPClientIdentifier identifier) {
@@ -350,7 +350,7 @@ int manager_configure_link_address(const IfNameIndex *ifnameidx, IPAddress *addr
                 }
         }
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_delete_link_address(const IfNameIndex *ifnameidx) {
@@ -377,7 +377,7 @@ int manager_delete_link_address(const IfNameIndex *ifnameidx) {
                 return r;
         }
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_configure_default_gateway(const IfNameIndex *ifnameidx, Route *rt) {
@@ -421,7 +421,7 @@ int manager_configure_default_gateway(const IfNameIndex *ifnameidx, Route *rt) {
                 return r;
         }
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_configure_route(const IfNameIndex *ifnameidx, Route *rt) {
@@ -463,7 +463,7 @@ int manager_configure_route(const IfNameIndex *ifnameidx, Route *rt) {
                 }
         }
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_remove_gateway_or_route(const IfNameIndex *ifnameidx, bool gateway) {
@@ -504,7 +504,7 @@ int manager_remove_gateway_or_route(const IfNameIndex *ifnameidx, bool gateway) 
                         (void) remove_key_from_config(network, "Route", "Metric");
         }
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_add_dns_server(const IfNameIndex *ifnameidx, DNSServers *dns, bool system) {
@@ -661,7 +661,7 @@ int manager_set_network_section_bool(const IfNameIndex *ifnameidx, const char *k
         if (r < 0)
                 return r;
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_set_dhcp_section(const IfNameIndex *ifnameidx, const char *k, bool v, bool dhcp4) {
@@ -681,7 +681,7 @@ int manager_set_dhcp_section(const IfNameIndex *ifnameidx, const char *k, bool v
         if (r < 0)
                 return r;
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_add_ntp_addresses(const IfNameIndex *ifnameidx, char **ntps) {
@@ -747,7 +747,7 @@ int manager_enable_ipv6(const IfNameIndex *ifnameidx, bool enable) {
         if (r < 0)
                 return r;
 
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_reload_network(void) {
@@ -755,7 +755,7 @@ int manager_reload_network(void) {
 }
 
 int manager_reconfigure_link(const IfNameIndex *ifnameidx) {
-        return dbus_reconfigure_link(ifnameidx->ifindex);
+        return dbus_network_reload(ifnameidx->ifindex);
 }
 
 int manager_write_wifi_config(const Network *n, const GString *config) {
