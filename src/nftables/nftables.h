4c937f8d2d411743f4916bdfdb6a85a7ffb51abe
Susant Sahani <ssahani@vmware.com>
2020-10-15 11:29:02 +0000

    nftable: Add support to add chains

--- src/nftables/nftables.h
+++ src/nftables/nftables.h
@@ -7,6 +7,7 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter/nf_tables.h>
 #include <libmnl/libmnl.h>
+#include <libnftnl/chain.h>
 #include <libnftnl/table.h>
 
 typedef enum NfProtoFamily {
@@ -25,8 +26,12 @@ typedef enum NfProtoFamily {
 const char *nft_family_to_name(int id);
 int nft_family_name_to_type(char *name);
 
-void nft_tables_unref(struct nftnl_table **t);
+void nft_table_unref(struct nftnl_table **t);
 
 int new_nft_table(int family, const char *name, struct nftnl_table **ret);
 int nft_add_table(int family, const char *name);
 int nft_get_tables(int family, char ***ret);
+
+void nft_chain_unref(struct nftnl_chain **c);
+int new_nft_chain(int family, const char *name, const char *table, struct nftnl_chain **ret);
+int nft_add_chain(int family, const char *table, const char *name);
