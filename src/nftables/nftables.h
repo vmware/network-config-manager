33e5d6a93386b3cc17931fa2365ec0e261b14563
Susant Sahani <ssahani@vmware.com>
2020-10-16 21:26:37 +0000

    nft: Add support to add fw port rule

--- src/nftables/nftables.h
+++ src/nftables/nftables.h
@@ -1,16 +1,16 @@
 /* SPDX-License-Identifier: Apache-2.0
  * Copyright © 2020 VMware, Inc.
  */
-
 #pragma once
+
+#include <gmodule.h>
 
 #include <linux/netfilter.h>
 #include <linux/netfilter/nf_tables.h>
 #include <libmnl/libmnl.h>
 #include <libnftnl/chain.h>
 #include <libnftnl/table.h>
-
-#include <gmodule.h>
+#include <libnftnl/rule.h>
 
 typedef enum NfProtoFamily {
         NF_PROTO_FAMILY_UNSPEC = NFPROTO_UNSPEC,
@@ -25,6 +25,31 @@ typedef enum NfProtoFamily {
         _NF_PROTO_FAMILY_INVALID = -1
 } NfProtoFamily;
 
+typedef enum NFPacketAction {
+        NF_PACKET_ACTION_DROP      = NF_DROP,
+        NF_PACKET_ACTION_ACCEPT    = NF_ACCEPT,
+        NF_PACKET_ACTION_NF_STOLEN = NF_STOLEN,
+        NF_PACKET_ACTION_NF_QUEUE  = NF_QUEUE,
+        NF_PACKET_ACTION_NF_REPEAT = NF_REPEAT,
+        NF_PACKET_ACTION_NF_STOP   = NF_STOP,
+        _NF_PACKET_ACTION_MAX,
+        _NF_PACKET_ACTION_INVALID = -1
+} NFPacketAction;
+
+typedef enum IPPacketPort {
+        IP_PACKET_PORT_SPORT,
+        IP_PACKET_PORT_DPORT,
+        _IP_PACKET_PORT_MAX,
+        _IP_PACKET_PORT_INVALID = -1
+} IPPacketPort;
+
+typedef enum IPPacketProtocol {
+        IP_PACKET_PROTOCOL_TCP = IPPROTO_TCP,
+        IP_PACKET_PROTOCOL_UDP = IPPROTO_UDP,
+        _IP_PACKET_PROTOCOL_MAX,
+        _IP_PACKET_PROTOCOL_INVALID = -1
+} IPPacketProtocol;
+
 typedef struct NFTNLTable {
         struct nftnl_table *table;
 
@@ -37,11 +62,18 @@ typedef struct NFTNLChain {
 
         char *name;
         char *table;
+
         int family;
 } NFTNLChain;
 
-const char *nft_family_to_name(int id);
-int nft_family_name_to_type(char *name);
+typedef struct NFTNLRule {
+        struct nftnl_rule *rule;
+
+        char *table;
+        char *chain;
+
+        int family;
+} NFTNLRule;
 
 void nft_table_unrefp(NFTNLTable **t);
 int nft_table_new(int family, const char *name, NFTNLTable **ret);
@@ -52,3 +84,21 @@ void nft_chain_unrefp(NFTNLChain **c);
 int nft_chain_new(int family, const char *name, const char *table, NFTNLChain **ret);
 int nft_add_chain(int family, const char *table, const char *name);
 int nft_get_chains(int family, GPtrArray **ret);
+
+void nft_rule_unrefp(NFTNLRule **c);
+int nft_rule_new(int family, const char *table, const char *chain, NFTNLRule **ret);
+int nft_configure_rule_port(int family, const char *table, const char *chain,
+                            IPPacketProtocol protocol, IPPacketPort port_type,
+                            uint16_t port, NFPacketAction action);
+
+const char *nft_packet_action_to_name(int id);
+int nft_packet_action_name_to_type(char *name);
+
+const char *ip_packet_port_type_to_name(int id);
+int ip_packet_port_name_to_type(char *name);
+
+const char *ip_packet_protocol_type_to_name(int id);
+int ip_packet_protcol_name_to_type(char *name);
+
+const char *nft_family_to_name(int id);
+int nft_family_name_to_type(char *name);
