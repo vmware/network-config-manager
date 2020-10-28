f2e608779af0551865f8eb56c1634900f84919f9
Susant Sahani <ssahani@vmware.com>
2020-10-28 11:46:43 +0000

    file: rename key_file_free -> key_file_freep

--- src/share/config-parser.h
+++ src/share/config-parser.h
@@ -14,7 +14,7 @@ int parse_config_file(const char *path, const char *section, const char *key, ch
 int parse_config_file_integer(const char *path, const char *section, const char *k, unsigned *ret);
 int parse_resolv_conf(char ***dns, char ***domains);
 
-static inline void key_file_free(GKeyFile **f) {
+static inline void key_file_freep(GKeyFile **f) {
         if (f && *f)
                 g_key_file_free(*f);
 }
