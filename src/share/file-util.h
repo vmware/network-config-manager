96c9e6f732aa252de8a745ccedc3bbc04d729f3d
Susant Sahani <ssahani@vmware.com>
2021-04-15 15:32:20 +0200

    share: Add glob pattern file match

--- src/share/file-util.h
+++ src/share/file-util.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <glob.h>
+
 int safe_mkdir_p_dir(const char* file_path) ;
 
 int set_file_permisssion(const char *path, const char *user);
@@ -12,3 +14,5 @@ int write_to_conf_file(const char *path, const GString *s);
 
 int read_one_line(const char *path, char **v);
 int write_one_line(const char *path, const char *v);
+
+int glob_files(const char *path, int flags, glob_t *ret);
