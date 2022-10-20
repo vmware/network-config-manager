f36c54c5e5c1f2f031c80a55a1f46934beb667e5
Susant Sahani <ssahani@vmware.com>
2022-10-20 19:36:26 +0530

    env: Allow NMCTL_BEAUTIFY=bool

--- src/share/ansi-color.c
+++ src/share/ansi-color.c
@@ -1,13 +1,15 @@
 /* Copyright 2022 VMware, Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
-#include "ansi-color.h"
 
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#include "ansi-color.h"
+#include "parse-util.h"
 
 #if defined(__GNUC__)
 #pragma GCC diagnostic push
@@ -18,18 +20,24 @@
 #endif
 
 void display_internal(bool enable_color, const char *color, const char *fmt, ...) {
-        va_list ap;
+       bool env_enable = true;
+       const char *e;
+       va_list ap;
 
-        va_start(ap, fmt);
+       e = getenv("NMCTL_BEAUTIFY");
+       if (e)
+               env_enable = parse_boolean(e);
 
-        if (!enable_color || (isatty(STDOUT_FILENO) < 0 && isatty(STDERR_FILENO) < 0)) {
-                vprintf(fmt, ap);
-                va_end(ap);
-                return;
-        }
+       va_start(ap, fmt);
 
-        printf("%s", color);
-        vprintf(fmt, ap);
-        printf("%s", ansi_color_reset());
-        va_end(ap);
+       if (!env_enable || !enable_color || (isatty(STDOUT_FILENO) < 0 && isatty(STDERR_FILENO) < 0)) {
+               vprintf(fmt, ap);
+               va_end(ap);
+               return;
+       }
+
+       printf("%s", color);
+       vprintf(fmt, ap);
+       printf("%s", ansi_color_reset());
+       va_end(ap);
 }
