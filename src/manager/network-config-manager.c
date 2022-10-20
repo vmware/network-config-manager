f77c434d9fdf07a489e29c72e5bf1681a7fc2ec4
Susant Sahani <ssahani@vmware.com>
2022-10-20 15:40:15 +0530

    ctl: Dsiaply DHCP server address and lifetime

--- src/manager/network-config-manager.c
+++ src/manager/network-config-manager.c
@@ -108,9 +108,13 @@ static void list_one_link_addresses(gpointer key, gpointer value, gpointer userd
                 printf("                   %s", c);
 
         r = network_parse_link_dhcp4_address(a->ifindex, &dhcp);
-        if (r >= 0 && string_has_prefix(c, dhcp))
-                printf(" (DHCPv4)\n");
-        else
+        if (r >= 0 && string_has_prefix(c, dhcp)) {
+                _auto_cleanup_ char *server = NULL, *life_time = NULL;
+                (void) network_parse_link_dhcp4_server_address(a->ifindex, &server);
+                (void) network_parse_link_dhcp4_address_lifetime(a->ifindex, &life_time);
+
+                printf(" (DHCPv4) via %s lifetime: %s seconds\n", server, life_time);
+        } else
                 printf("\n");
 }
 
