e5fca51e7d438a8b51cdf0340ca6f9bf4c23b5ca
Susant Sahani <ssahani@vmware.com>
2022-11-13 14:26:53 +0530

    Refacor list devs and status links

--- src/manager/ctl-display.c
+++ src/manager/ctl-display.c
@@ -62,34 +62,6 @@ static void link_state_to_color(const char *state, const char **on) {
                 *on = ansi_color_reset();
 }
 
-static void display_links_info(gpointer data_ptr, gpointer ignored) {
-        const char *setup_color, *operational_color, *operstates, *operstates_color;
-        _auto_cleanup_ char *setup = NULL, *operational = NULL;
-        Link *link = NULL;
-
-        setup_color = operational_color = operstates = operstates_color = ansi_color_reset();
-
-        link = data_ptr;
-
-        (void) network_parse_link_setup_state(link->ifindex, &setup);
-        (void) network_parse_link_operational_state(link->ifindex, &operational);
-        operstates = link_operstates_to_name(link->operstate);
-
-        if (setup)
-                link_state_to_color(setup, &setup_color);
-        if (operational)
-                link_state_to_color(operational, &operational_color);
-        if (operstates)
-                link_state_to_color(operstates, &operstates_color);
-
-        display(arg_beautify, ansi_color_bold(), "%d     ", link->ifindex);
-        display(arg_beautify, ansi_color_bold_cyan(), "%-10s ", link->name);
-        display(arg_beautify, ansi_color_blue_magenta(), "%-10s ", arphrd_to_name(link->iftype));
-        display(arg_beautify, operstates_color, "%-10s ", operstates);
-        display(arg_beautify, operational_color, "%-14s ", string_na(operational));
-        display(arg_beautify, setup_color, "%-16s\n", string_na(setup));
-}
-
 static int list_links(int argc, char *argv[]) {
         _cleanup_(links_unrefp) Links *h = NULL;
         int r;
@@ -99,7 +71,7 @@ static int list_links(int argc, char *argv[]) {
                return r;
 
         if (arg_beautify)
-                printf("%5s %-10s %-10s %-10s %-14s %-16s\n",
+                printf("%s %8s %10s %11s %15s %9s\n",
                         "INDEX",
                         "LINK",
                         "TYPE",
@@ -107,7 +79,32 @@ static int list_links(int argc, char *argv[]) {
                         "OPERATIONAL",
                         "SETUP");
 
-        g_list_foreach(h->links, display_links_info, NULL);
+        for (GList *i = h->links; i; i = g_list_next (i)) {
+                const char *setup_color, *operational_color, *operstates, *operstates_color;
+                _auto_cleanup_ char *setup = NULL, *operational = NULL;
+                Link *link = (Link *) i->data;
+
+                setup_color = operational_color = operstates = operstates_color = ansi_color_reset();
+
+                (void) network_parse_link_setup_state(link->ifindex, &setup);
+                (void) network_parse_link_operational_state(link->ifindex, &operational);
+                operstates = link_operstates_to_name(link->operstate);
+
+                if (setup)
+                        link_state_to_color(setup, &setup_color);
+                if (operational)
+                        link_state_to_color(operational, &operational_color);
+                if (operstates)
+                        link_state_to_color(operstates, &operstates_color);
+
+                display(arg_beautify, ansi_color_bold(), "%-8d", link->ifindex);
+                display(arg_beautify, ansi_color_bold_cyan(), "  %-10s ", link->name);
+                display(arg_beautify, ansi_color_blue_magenta(), "%-10s ", arphrd_to_name(link->iftype));
+                display(arg_beautify, operstates_color, "%-9s ", operstates);
+                display(arg_beautify, operational_color, "%-15s ", string_na(operational));
+                display(arg_beautify, setup_color, "%-20s\n", string_na(setup));
+        }
+
         return 0;
 }
 
