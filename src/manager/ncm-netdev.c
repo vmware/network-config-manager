63d033933669e486fdf6d42b2979e7cc5101db09
Susant Sahani <ssahani@vmware.com>
2022-11-15 07:39:44 +0530

    Fix parsing vrf

--- src/manager/ncm-netdev.c
+++ src/manager/ncm-netdev.c
@@ -443,13 +443,17 @@ _public_ int ncm_create_vrf(int argc, char *argv[]) {
         uint32_t table;
         int r;
 
-        if (string_equal(argv[2], "table")) {
-                r = parse_uint32(argv[3], &table);
-                if (r < 0) {
-                        log_warning("Failed to parse table id '%s' for '%s': %s", argv[3], argv[1], g_strerror(EINVAL));
-                        return r;
+        for (int i = 1; i < argc; i++) {
+                if (string_equal(argv[i], "table") || string_equal(argv[i], "t")) {
+                        parse_next_arg(argv, argc, i);
+
+                        r = parse_uint32(argv[3], &table);
+                        if (r < 0) {
+                                log_warning("Failed to parse table id='%s' for '%s': %s", argv[i], argv[1], g_strerror(-r));
+                                return r;
+                        }
+                        have_table = true;
                 }
-                have_table = true;
         }
 
         if (!have_table) {
