e5e0e4babeff714ac54a811b38bd62bbec9cd4db
Susant Sahani <ssahani@vmware.com>
2023-03-24 19:59:36 +0530

    CI: Add test for packets for slave for bond

--- tests/network-config-manager-tests.py
+++ tests/network-config-manager-tests.py
@@ -438,6 +438,7 @@ class TestNetworkConfigManagerYAML:
         assert(parserb.get('Bond', 'DownDelaySec') == '15')
         assert(parserb.get('Bond', 'LearnPacketIntervalSec') == '32')
         assert(parserb.get('Bond', 'ResendIGMP') == '45')
+        assert(parserb.get('Bond', 'PacketsPerSlave') == '11')
 
         parserc = configparser.ConfigParser()
         parserc.read(os.path.join(networkd_unit_file_path, '10-bond0.network'))
