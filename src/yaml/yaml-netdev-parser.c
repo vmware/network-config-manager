19dda4ad86008a055195680dc1a7dc5735573c19
Susant Sahani <ssahani@vmware.com>
2023-03-24 20:31:53 +0530

    yaml: Add support for ARPIPTargets

--- src/yaml/yaml-netdev-parser.c
+++ src/yaml/yaml-netdev-parser.c
@@ -94,6 +94,7 @@ static int yaml_detect_tunnel_kind(yaml_document_t *dp, yaml_node_t *node) {
 
 static ParserTable parser_bond_vtable[] = {
         { "interfaces",            CONF_TYPE_NETDEV_BOND, parse_yaml_sequence,       offsetof(Bond, interfaces)},
+        { "arp-ip-targets",        CONF_TYPE_NETDEV_BOND, parse_yaml_sequence,       offsetof(Bond, arp_ip_targets)},
         { "mode",                  CONF_TYPE_NETDEV_BOND, parse_yaml_bond_mode,      offsetof(Bond, mode)},
         { "lacp-rate",             CONF_TYPE_NETDEV_BOND, parse_yaml_bond_lacp_rate, offsetof(Bond, lacp_rate)},
         { "mii-monitor-interval",  CONF_TYPE_NETDEV_BOND, parse_yaml_uint64,         offsetof(Bond, mii_monitor_interval)},
