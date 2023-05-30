03786b9a3ab48adb759e3a7e942d922e7d05c5eb
Susant Sahani <ssahani@gmail.com>
2023-05-30 15:33:23 +0530

    key file: Introduce contains

--- src/share/config-parser.h
+++ src/share/config-parser.h
@@ -16,7 +16,9 @@ int parse_key_file(const char *path, KeyFile **ret);
 int display_key_file(const KeyFile *k);
 
 bool config_exists(const char *path, const char *section, const char *k, const char *v);
+bool config_contains(const char *path, const char *section, const char *k, const char *v);
 bool key_file_config_exists(const KeyFile *kf, const char *s, const char *k, const char *v);
+bool key_file_config_contains(const KeyFile *kf, const char *s, const char *k, const char *v);
 char *key_file_config_get(const KeyFile *key_file, const char *section, const char *k);
 
 int parse_config_file(const char *path, const char *section, const char *key, char **ret);
