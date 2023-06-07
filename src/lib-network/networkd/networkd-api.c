f0090788b43f3494a4fd7bac653e373e778e4f20
Susant Sahani <ssahani@gmail.com>
2023-06-07 14:14:15 +0530

    network: Introduce network_parse_address_state

--- src/lib-network/networkd/networkd-api.c
+++ src/lib-network/networkd/networkd-api.c
@@ -32,6 +32,10 @@ int network_parse_carrier_state(char **ret) {
         return network_parse_string("CARRIER_STATE", ret);
 }
 
+int network_parse_address_state(char **ret) {
+        return network_parse_string("ADDRESS_STATE", ret);
+}
+
 static int network_parse_strv(const char *key, char ***ret) {
         _auto_cleanup_ char *s = NULL;
         int r;
