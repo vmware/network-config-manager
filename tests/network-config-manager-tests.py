0cde109bfde81a0994dbaa423049e040542963b6
Susant Sahani <ssahani@vmware.com>
2023-12-18 09:52:16 +0530

    CI: Add test for JSON dns dev

--- tests/network-config-manager-tests.py
+++ tests/network-config-manager-tests.py
@@ -831,6 +831,9 @@ class TestCLINetwork:
     def test_cli_show_dns_json(self):
         subprocess.check_call("nmctl dns -j", text=True, shell = True)
 
+    def test_cli_show_dns_dev_json(self):
+        subprocess.check_call("nmctl dns dev 2 -j", text=True, shell = True)
+
     def test_cli_show_domains(self):
         subprocess.check_call("nmctl add-domain dev test99 domains domain1 domain2", shell = True)
         subprocess.check_call("nmctl domain", text=True, shell = True)
