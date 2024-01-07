bba7eacf11a5a2ada583bdd24acd1c76a5a5bf7c
Susant Sahani <ssahani@gmail.com>
2024-01-07 20:00:11 +0530

    CI: set-dynaic - add test for DHCP IAID

--- tests/cmocka/basic.c
+++ tests/cmocka/basic.c
@@ -717,6 +717,29 @@ static void test_set_dynamic_dhcp_ipv4_ipv4_ra_dhcp4_client_identifier(void **st
     assert_true(key_file_config_exists(key_file, "Network", "DHCP", "yes"));
 }
 
+static void test_set_dynamic_dhcp_ipv4_ipv4_ra_dhcp4_client_identifier_dhcp_iaid(void **state) {
+    _cleanup_(key_file_freep) KeyFile *key_file = NULL;
+    int r;
+
+    assert_true(system(" set-dynamic dev test99 dhcp yes dhcp4-client-id mac dhcp4-iaid 0x12345 dhcp6-iaid 0x12346") >= 0);
+
+    r = parse_key_file("/etc/systemd/network/10-test99.network", &key_file);
+    assert_true(r >= 0);
+
+    display_key_file(key_file);
+
+    assert_true(key_file_config_exists(key_file, "Match", "Name", "test99"));
+
+    assert_true(key_file_config_exists(key_file, "DHCPv4", "ClientIdentifier", "mac"));
+    assert_true(key_file_config_exists(key_file, "DHCPv4", "IAID", "0x12345"));
+
+    assert_true(key_file_config_exists(key_file, "DHCPv6", "IAID", "0x12346"));
+
+    assert_true(key_file_config_exists(key_file, "Network", "LinkLocalAddressing", "ipv6"));
+    assert_true(key_file_config_exists(key_file, "Network", "IPv6AcceptRA", "yes"));
+    assert_true(key_file_config_exists(key_file, "Network", "DHCP", "yes"));
+}
+
 static void test_additional_gw_source_routing(void **state) {
     _cleanup_(key_file_freep) KeyFile *key_file = NULL;
     int r;
@@ -1510,6 +1533,7 @@ int main(void) {
         cmocka_unit_test (test_add_dhcp4_server_static_address),
         cmocka_unit_test (test_remove_dhcp4_server_static_address),
         cmocka_unit_test (test_set_dynamic_dhcp_ipv4_ipv4_ra_dhcp4_client_identifier),
+        cmocka_unit_test (test_set_dynamic_dhcp_ipv4_ipv4_ra_dhcp4_client_identifier_dhcp_iaid),
         cmocka_unit_test (test_yaml_add_dhcp4_server_static_address),
         cmocka_unit_test (test_yaml_add_sriov),
     };
