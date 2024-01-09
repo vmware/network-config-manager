4cfa072e1ff0ed487f4b0fad8a67e58a2ddfc5a8
Susant Sahani <ssahani@gmail.com>
2024-01-09 10:51:42 +0530

    CI: Add test for set-ipv4

--- tests/cmocka/basic.c
+++ tests/cmocka/basic.c
@@ -1371,6 +1371,8 @@ int main(void) {
         cmocka_unit_test (test_set_network_dhcp_ipv4_ipv4_ra_dhcp4_client_identifier_dhcp_iaid_static_address_gw_dns),
         cmocka_unit_test (test_yaml_add_dhcp4_server_static_address),
         cmocka_unit_test (test_yaml_add_sriov),
+        cmocka_unit_test (test_set_ipv4_dhcp_yes_with_static),
+        cmocka_unit_test (test_set_ipv4_dhcp_no_with_static),
         cmocka_unit_test (test_set_network_address_gw),
         cmocka_unit_test (test_set_network_address_gw_dns),
         cmocka_unit_test (test_set_network_address_gw_dns_keep_yes),
--- tests/cmocka/set-network.h
+++ tests/cmocka/set-network.h
@@ -3,6 +3,9 @@
  */
 #pragma once
 
+void test_set_ipv4_dhcp_yes_with_static(void **state);
+void test_set_ipv4_dhcp_no_with_static(void **state);
+
 void test_set_static_address_gw(void **state);
 void test_set_static_address_gw_dns(void **state);
 void test_set_static_address_gw_dns_keep_yes(void **state);
